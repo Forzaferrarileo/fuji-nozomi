fa7d589add482b59a0e16bcea0a9b764f43463b4
Forzaferrarileo <leonardo.ferroro@gmail.com>
2013-01-05 23:36:55 +0100

    Added 2D gpu scaling , fixed gpu oc and io_fraction

--- kernel/arch/arm/mach-msm/devices-msm8x60.c
+++ kernel/arch/arm/mach-msm/devices-msm8x60.c
@@ -685,6 +685,16 @@ static struct msm_bus_vectors grp2d0_init_vectors[] = {
 	},
 };
 
+
+static struct msm_bus_vectors grp2d0_low_vectors[] = {
+	{
+		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
+		.dst = MSM_BUS_SLAVE_EBI_CH0,
+		.ab = 0,
+		.ib = KGSL_CONVERT_TO_MBPS(960),
+	},
+};
+
 static struct msm_bus_vectors grp2d0_max_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE0,
@@ -698,6 +708,10 @@ static struct msm_bus_paths grp2d0_bus_scale_usecases[] = {
 	{
 		ARRAY_SIZE(grp2d0_init_vectors),
 		grp2d0_init_vectors,
+	},
+	{
+		ARRAY_SIZE(grp2d0_low_vectors),
+		grp2d0_low_vectors,
 	},
 	{
 		ARRAY_SIZE(grp2d0_max_vectors),
@@ -720,6 +734,15 @@ static struct msm_bus_vectors grp2d1_init_vectors[] = {
 	},
 };
 
+static struct msm_bus_vectors grp2d1_low_vectors[] = {
+	{
+		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
+		.dst = MSM_BUS_SLAVE_EBI_CH0,
+		.ab = 0,
+		.ib = KGSL_CONVERT_TO_MBPS(960),
+	},
+};
+
 static struct msm_bus_vectors grp2d1_max_vectors[] = {
 	{
 		.src = MSM_BUS_MASTER_GRAPHICS_2D_CORE1,
@@ -733,6 +756,10 @@ static struct msm_bus_paths grp2d1_bus_scale_usecases[] = {
 	{
 		ARRAY_SIZE(grp2d1_init_vectors),
 		grp2d1_init_vectors,
+	},
+	{
+		ARRAY_SIZE(grp2d1_low_vectors),
+		grp2d1_low_vectors,
 	},
 	{
 		ARRAY_SIZE(grp2d1_max_vectors),
@@ -782,32 +809,32 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 4,
-			.io_fraction = 33,
-		},
+			.io_fraction = 0,
+		},	
 		{
 			.gpu_freq = 300000000,
 			.bus_freq = 4,
-			.io_fraction = 33,
-		},	
-		{
-			.gpu_freq = 266667000,
-			.bus_freq = 4,
-			.io_fraction = 33,
+			.io_fraction = 15,
 		},
 		{
-			.gpu_freq = 228571000,
+			.gpu_freq = 266667000,
 			.bus_freq = 3,
 			.io_fraction = 33,
 		},
 		{
+			.gpu_freq = 228571000,
+			.bus_freq = 2,
+			.io_fraction = 50,
+		},
+		{
 			.gpu_freq = 200000000,
 			.bus_freq = 2,
-			.io_fraction = 100,
+			.io_fraction = 65,
 		},
 		{
 			.gpu_freq = 177778000,
 			.bus_freq = 1,
-			.io_fraction = 100,
+			.io_fraction = 85,
 		},
 		{
 			.gpu_freq = 145455000,
@@ -858,16 +885,14 @@ static struct resource kgsl_2d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 228571000,
-			.bus_freq = 1,
+			.gpu_freq = 266667000,
+			.bus_freq = 2,
+			.io_fraction = 50,
 		},
 		{
 			.gpu_freq = 200000000,
 			.bus_freq = 1,
-		},
-		{
-			.gpu_freq = 128000000,
-			.bus_freq = 1,
+			.io_fraction = 100,
 		},
 		{
 			.gpu_freq =  64000000,
@@ -875,7 +900,7 @@ static struct kgsl_device_platform_data kgsl_2d0_pdata = {
 		},
 	},
 	.init_level = 0,
-	.num_levels = 4,
+	.num_levels = 3,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/10,
 	.nap_allowed = true,
@@ -913,15 +938,17 @@ static struct resource kgsl_2d1_resources[] = {
 static struct kgsl_device_platform_data kgsl_2d1_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 200000000,
-			.bus_freq = 1,
-		},
-		{
-			.gpu_freq = 128000000,
-			.bus_freq = 1,
+			.gpu_freq = 266667000,
+			.bus_freq = 2,
+			.io_fraction = 50,
 		},
 		{
 			.gpu_freq = 200000000,
+			.bus_freq = 1,
+			.io_fraction = 100,
+		},
+		{
+			.gpu_freq = 64000000,
 			.bus_freq = 0,
 		},
 	},
