f6d4dad3177b51819f8d6bee1a021ee920b52184
Forzaferrarileo <leonardo.ferroro@gmail.com>
2013-01-11 18:00:23 +0100

    ARM: topology: Add a cpu_power function

--- kernel/arch/arm/include/asm/topology.h
+++ kernel/arch/arm/include/asm/topology.h
@@ -18,8 +18,8 @@ extern struct cputopo_arm cpu_topology[NR_CPUS];
 
 #define topology_physical_package_id(cpu)	(cpu_topology[cpu].socket_id)
 #define topology_core_id(cpu)		(cpu_topology[cpu].core_id)
-#define topology_core_cpumask(cpu)	(&(cpu_topology[cpu].core_sibling))
-#define topology_thread_cpumask(cpu)	(&(cpu_topology[cpu].thread_sibling))
+#define topology_core_cpumask(cpu)	(&cpu_topology[cpu].core_sibling)
+#define topology_thread_cpumask(cpu)	(&cpu_topology[cpu].thread_sibling)
 
 #define mc_capable()	(cpu_topology[0].socket_id != -1)
 #define smt_capable()	(cpu_topology[0].thread_id != -1)
@@ -28,13 +28,16 @@ void init_cpu_topology(void);
 void store_cpu_topology(unsigned int cpuid);
 const struct cpumask *cpu_coregroup_mask(int cpu);
 
+void set_power_scale(unsigned int cpu, unsigned int power);
 #else
 
-#define init_cpu_topology() {};
-#define store_cpu_topology(cpuid) {};
+static inline void init_cpu_topology(void) { }
+static inline void store_cpu_topology(unsigned int cpuid) { }
 
+static inline void set_power_scale(unsigned int cpu, unsigned int power) { }
 #endif
 
 #include <asm-generic/topology.h>
 
 #endif /* _ASM_ARM_TOPOLOGY_H */
+
